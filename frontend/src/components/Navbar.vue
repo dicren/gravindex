b0d377edf64003c3e097e7342f3ff3f59a1e81b8
dicren <me@dicren.com>
2020-03-31 15:15:42 +0200

    Estilo de la cabecera mejorado

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -5,12 +5,12 @@
         <img src="@/assets/logo.png" />
       </router-link>
 
-      <router-link class="navbar-item" :to="{ name: 'home' }">
-        Biblioteca&nbsp;<span class="is-hidden-mobile">gravinera</span>
+      <router-link class="navbar-item main" :to="{ name: 'home' }">
+        Biblioteca<span class="is-hidden-mobile"> gravinera</span>
       </router-link>
 
       <router-link class="navbar-item" :to="{ name: 'episodes' }">
-        <span class="is-hidden-mobile">Lista de </span>&nbsp;episodios
+        <span class="is-hidden-mobile">Lista de </span>episodios
       </router-link>
 
       <router-link class="navbar-item" :to="{ name: 'creatorSearch' }">
@@ -36,6 +36,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.router-link-active:not(.main),
+.router-link-exact-active {
+  color: $brown-gravinero !important;
+}
+.navbar-item:focus {
+  background-color: transparent !important;
+}
+
+.navbar-item {
+  transition: color 0.3s ease;
+}
+
 @include mobile {
   .navbar-brand {
     margin-bottom: -3rem;
