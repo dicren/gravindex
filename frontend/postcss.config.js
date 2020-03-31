22124263a5406c94a00b231cbde541f1ff5f01df
dicren <me@dicren.com>
2020-03-31 14:35:10 +0200

    Optimizado el css con purgecss

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,5 +1,27 @@
 module.exports = {
-  plugins: {
-    autoprefixer: {},
-  },
+  plugins: [
+    require("@fullhuman/postcss-purgecss")({
+      content: [`./public/**/*.html`, `./src/**/*.vue`],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(
+          /<style[^]+?<\/style>/gi,
+          ""
+        );
+        return (
+          contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) ||
+          []
+        );
+      },
+      whitelist: [],
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /data-v-.*/,
+        /dropdown.*/, // Para autocomplete
+        /autocomplete.*/, // Para autocomplete
+        /noUi.*/, // Para el slider del editor
+      ],
+    }),
+  ],
 };
